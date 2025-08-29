b5a574b9380305459e801f046614387a7674bea2
weeger <romain.weeger@gmail.com>
2025-08-29 15:50:38 +0200

    Docstring

--- tests/resources/item/class/basic_calculator.py
+++ tests/resources/item/class/basic_calculator.py
@@ -4,5 +4,10 @@ class Calculator:
     lastResult: int = 0  # Stores the result of the last operation performed.
 
     def add(self, a: int, b: int) -> int:
-        """Calculate the sum of two ints."""
+        """Calculate the sum of two ints.
+
+        :param a: The first operand.
+        :param b: The second operand.
+        :return: The sum of the two numbers.
+        """
         pass
