91f33eaaccdd13d56b680456639f5aee16f1e7e4
weeger <romain.weeger@gmail.com>
2025-08-29 15:43:25 +0200

    Docstring

--- tests/resources/item/function/complex_function.py
+++ tests/resources/item/function/complex_function.py
@@ -1,3 +1,9 @@
 def processData(data: list, config: list = [], callback: typing.Optional[typing.Callable] = None) -> list:
-    """Process an array of data with optional configuration."""
+    """Process an array of data with optional configuration.
+
+    :param data: The data to process
+    :param config: Optional configuration parameters
+    :param callback: Optional callback for custom processing
+    :return: array
+    """
     pass
