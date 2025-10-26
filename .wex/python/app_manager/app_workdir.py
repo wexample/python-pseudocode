06ba15d3963621755fb98825549ea0cc494ec7fe
weeger <romain.weeger@gmail.com>
2025-10-26 09:42:33 +0100

    Fix

--- .wex/python/app_manager/app_workdir.py
+++ .wex/python/app_manager/app_workdir.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-from wexample_config.const.types import DictConfig
 from wexample_wex_addon_dev_python.workdir.python_package_workdir import PythonPackageWorkdir
 
 
 class AppWorkdir(PythonPackageWorkdir):
-    def prepare_value(self, raw_value: DictConfig | None = None) -> DictConfig:
-        return raw_value
+    pass
